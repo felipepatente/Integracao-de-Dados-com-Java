b03bbf2f848e8c5589313d645813949fc5592c82
felipepatente <felipepatente@yahoo.com.br>
2017-10-11 13:56:02 -0300

    Criando os atributos necessários para a leitura

--- IntegracaoPokemon/src/negocios/PokemonCSV.java
+++ IntegracaoPokemon/src/negocios/PokemonCSV.java
@@ -1,5 +1,48 @@
 package negocios;
 
 public class PokemonCSV {
+	
+	private String altura;
+	private String categoria;
+	private String descricao;
+	private String habilidade;
+	private String nome;
+	private String sexo;
+	public String getAltura() {
+		return altura;
+	}
+	public void setAltura(String altura) {
+		this.altura = altura;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getHabilidade() {
+		return habilidade;
+	}
+	public void setHabilidade(String habilidade) {
+		this.habilidade = habilidade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 
 }
