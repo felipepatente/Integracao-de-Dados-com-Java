0f8c457e794169418f070925291fce20d9b0b3e4
felipepatente <felipepatente@yahoo.com.br>
2017-10-12 16:13:18 -0300

    Criando um método para ler arquivo csv

--- IntegracaoPokemon/src/negocios/Extracao.java
+++ IntegracaoPokemon/src/negocios/Extracao.java
@@ -1,5 +1,40 @@
 package negocios;
 
-public class Extracao {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import objetos.PokemonCSV;
+import objetos.PokemonJSON;
+import objetos.PokemonXML;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+public class Extracao {
+	
+	private ArrayList<PokemonCSV> listaCSV;
+	
+	public Extracao(){
+		listaCSV = new ArrayList<PokemonCSV>();
+	}
+	
+	public void extrairArquivoCSV() throws JsonProcessingException, IOException{
+
+		CsvMapper mapper = new CsvMapper();			
+		mapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
+		CsvSchema schema = CsvSchema.emptySchema().withColumnSeparator(';').withHeader().withoutQuoteChar();
+		File csvFile = new File("./pokebase.csv"); 
+		MappingIterator<PokemonCSV> it = mapper.readerFor(PokemonCSV.class).with(schema).readValues(csvFile);
+			
+		while (it.hasNext()) {
+			PokemonCSV csv = it.next();
+			listaCSV.add(csv);			
+		}
+	}
 }
