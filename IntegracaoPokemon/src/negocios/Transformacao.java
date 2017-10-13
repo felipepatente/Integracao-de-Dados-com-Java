2e2a9eddfcc5b4dcbea5e3d23beb3d381a63a722
felipepatente <felipepatente@yahoo.com.br>
2017-10-13 11:58:55 -0300

    Método responsável por passar os de uma lista XML para outra

--- IntegracaoPokemon/src/negocios/Transformacao.java
+++ IntegracaoPokemon/src/negocios/Transformacao.java
@@ -1,6 +1,21 @@
 package negocios;
 
+import java.util.ArrayList;
+
+import objetos.Pokemon;
+import objetos.PokemonXML;
+
 public class Transformacao {
 	
+	private ArrayList<Pokemon> listaPokemon;
 	
+	public Transformacao(){
+		listaPokemon = new ArrayList<Pokemon>();
+	}
+	
+	public void transformarXML(ArrayList<PokemonXML> listaXML){
+		
+		
+		
+	}
 }
